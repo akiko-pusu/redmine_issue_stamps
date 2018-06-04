b9aefb82e321e4abb59a3d77c136804965761cba
Akiko Takano <akiko.pusu@gmail.com>
2018-06-05 08:29:56 +0900

    Add first source code via generated plugin generator.

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_issue_stamps do
+  name 'Redmine Issue Stamps plugin'
+  author 'Akiko Takano'
+  description 'This is a plugin for Redmine, which enables to display approval stamps on issue description page.'
+  version '0.0.1'
+  url ''
+  author_url 'https://github.com/akiko-pusu'
+end
